2e68f5a2c6e521128d025e68dc1f287fee58a81a
bunny13 <shashank13verma@gmail.com>
2019-09-05 09:54:48 -0400

    Update Product.js

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,13 +1,74 @@
-import React from "react";
-import PRODUCTS from  "../../dummy-data/products";
- 
-const initalState =  {
-    availableProducts: PRODUCTS,
-    userProducts: PRODUCTS.filter(product => product.ownerId === 'u1')
-}
+import PRODUCTS from '../../dummy-data/products';
+import { DELETE_PRODUCT, CREATE_PRODUCT, UPDATE_PRODUCT, SET_PRODUCTS } from '../actions/actionTypes';
+import Product from "../../models/product";
 
-const reducer = (state = initalState, action) => {
-    return state;
-}
+const initialState = {
+  availableProducts: [],
+  userProducts: []
+};
 
-export default reducer;
+export default (state = initialState, action) => {
+  switch (action.type) {
+
+    case SET_PRODUCTS:
+        const loadedData = action.prodData;
+        return {
+            ...state,
+            availableProducts:loadedData,
+            userProducts:loadedData.filter(prod => prod.ownerId === 'u1')
+        }
+
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        userProducts: state.userProducts.filter(
+          product => product.id !== action.pid
+        ),
+        availableProducts: state.availableProducts.filter(
+            product => product.id !== action.pid
+          ),
+      };
+      case UPDATE_PRODUCT:
+          return {
+              ...state,
+              userProducts: state.userProducts.map(prod => {
+                if(prod.id === action.prodData.id) {
+                    return {
+                        ...prod,
+                        title: action.prodData.title,
+                        description: action.prodData.description,
+                        imageUrl: action.prodData.imageUrl
+                    }
+                }
+                return prod
+              }),
+              availableProducts: state.availableProducts.map(prod => {
+                if(prod.id === action.prodData.id) {
+                    return {
+                        ...prod,
+                        title: action.prodData.title,
+                        description: action.prodData.description,
+                        imageUrl: action.prodData.imageUrl
+                    }
+                }
+                return prod
+              })
+          }
+      case CREATE_PRODUCT:
+          const newProduct = new Product(
+              action.prodData.id,
+              "u1",
+              action.prodData.imageUrl,
+              action.prodData.title,
+              action.prodData.description,
+              action.prodData.price
+          )
+          return {
+              ...state,
+              userProducts:state.userProducts.concat(newProduct),
+              availableProducts: state.userProducts.concat(newProduct)
+          }
+      default:
+        return state;
+  }
+};
