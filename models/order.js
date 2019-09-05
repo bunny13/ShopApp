5c6ea983f4988f487ca05de9c4fad1977bd8e89e
bunny13 <shashank13verma@gmail.com>
2019-09-05 09:44:37 -0400

    Create Order.js

---
+++ models/order.js
@@ -0,0 +1,18 @@
+import moment from "moment";
+
+class Order {
+    constructor(id, items, totalAmt, orderedDate){
+        console.log("i amhrer");
+        this.id = id;
+        this.items = items;
+        this.totalAmt = totalAmt;
+        this.orderedDate = orderedDate;
+    }
+
+    get getDate(){
+        return moment(this.orderedDate).format("MMMM Do YYYY, hh:mm");
+    }
+
+}
+
+export default Order;
